8c3e3ba827462cec21487fa0519212809b542efe
vang838 <vang838@uwm.edu>
2026-09-10 05:53:49 +0000

    fix: header comment added

--- Homework1/myhead.c
+++ Homework1/myhead.c
@@ -1,10 +1,16 @@
-// TODO: Add header comment; header files and helper functions as needed
+/*************************************************************
+Author: Matthew Vang
+Created: September 9th, 2026
+
+This program implements a simplified version of the Unix "head" command.
+Program accepts a single file name as a cli argument and prints the first 6 lines of the file directly to terminal.
+*************************************************************/
 
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-    // TODO: implement myhead
+    // implement myhead
     if(argc != 2) {
         printf("Usage: %s <filename>\n", argv[0]);
         return 1;
@@ -19,7 +25,7 @@ int main(int argc, char *argv[]) {
     int count = 0;
     char buff[256];
     while(count < 6 && fgets(buff, sizeof buff, file)) {
-        fputs(buff, stdout);
+        fputs(buff, stdout); // print line to terminal directly
         count++;
     }
     return 0;
